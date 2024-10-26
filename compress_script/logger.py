e22138dd3616b9f774f5e2df3c93c0959d865a08
scdhh <wallfjjd@gmail.com>
2024-10-27 00:31:51 +0800

    🐛 修复日志颜色

--- compress_script/logger.py
+++ compress_script/logger.py
@@ -1,8 +1,16 @@
 from loguru import logger
 from rich.console import Console
+from rich.theme import Theme
 from richuru import install  # type: ignore[import-untyped]
 
-console = Console()
+console = Console(
+    theme=Theme(
+        {
+            'logging.level.success': 'green',
+            'logging.level.trace': 'bright_black',
+        }
+    )
+)
 
 install(rich_console=console)
 
